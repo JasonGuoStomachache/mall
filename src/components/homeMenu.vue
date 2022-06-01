d3ff661fc06a6011a8ef7520f53b946d3dabb734
Stomachache <3508822603@qq.com>
2022-06-01 14:23:13 +0800

    导航栏重新规划

--- src/components/homeMenu.vue
+++ src/components/homeMenu.vue
@@ -4,34 +4,34 @@
     background-color="#545c64"
     text-color="#fff"
   >
-    <el-submenu index="1">
+    <el-menu-item index="1">
+      <el-icon><Apple /></el-icon>
+      <span>首页</span>
+    </el-menu-item>
+    <el-submenu index="2">
       <template #title>
-        <el-icon><location /></el-icon>
-        <span>Navigator One</span>
+        <el-icon><Menu /></el-icon>
+        <span>分类</span>
       </template>
       <el-menu-item-group title="Group One">
-        <el-menu-item index="1-1">item one</el-menu-item>
-        <el-menu-item index="1-2">item one</el-menu-item>
+        <el-menu-item index="2-1">item one</el-menu-item>
+        <el-menu-item index="2-2">item one</el-menu-item>
       </el-menu-item-group>
       <el-menu-item-group title="Group Two">
-        <el-menu-item index="1-3">item three</el-menu-item>
+        <el-menu-item index="2-3">item three</el-menu-item>
       </el-menu-item-group>
-      <el-sub-menu index="1-4">
+      <el-sub-menu index="2-4">
         <template #title>item four</template>
-        <el-menu-item index="1-4-1">item one</el-menu-item>
+        <el-menu-item index="2-4-1">item one</el-menu-item>
       </el-sub-menu>
     </el-submenu>
-    <el-menu-item index="2">
-      <el-icon><icon-menu /></el-icon>
-      <span>Navigator Two</span>
-    </el-menu-item>
-    <el-menu-item index="3" disabled>
-      <el-icon><document /></el-icon>
-      <span>Navigator Three</span>
+    <el-menu-item index="3">
+      <el-icon><ShoppingCart /></el-icon>
+      <span>购物车</span>
     </el-menu-item>
     <el-menu-item index="4">
       <el-icon><setting /></el-icon>
-      <span>Navigator Four</span>
+      <span>用户中心</span>
     </el-menu-item>
   </el-menu>
 </template>
